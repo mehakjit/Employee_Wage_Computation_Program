688ddd4ed372947579eb8d2b74a3378add75d3f5
mehakjit <mehakjitsingh@gmail.com>
2020-09-24 13:03:03 +0530

    [Mehakjit]Created: UC1 To Check Employee Present or Absent

--- src/com/capgemini/Employee_Wage_Management.java
+++ src/com/capgemini/Employee_Wage_Management.java
@@ -3,6 +3,12 @@ package com.capgemini;
 public class Employee_Wage_Management {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int Full_time = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == Full_time)
+			System.out.println("Employee is present");
+		else 
+			System.out.println("Employee is Absent");
 	}
-}
+} 
+
